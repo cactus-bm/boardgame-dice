191290a991ead9882ea0dda636c70ddcb0f0065c
James Robinson <james@cactus.bm>
2020-11-02 05:38:49 -0400

    Added a Roll Tracker component

--- boardgame dice/GameStateModel.swift
+++ boardgame dice/GameStateModel.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+class GameStateModel {
+    let players: PlayersStateModel
+    let turnTracker: TurnTrackerStateModel
+    let frequencyCounter: FrequencyCounterStateModel
+    let rollTracker: RollTrackerStateModel
+    var settingsMode: Bool = false
+    
+    init() {
+        self.players = PlayersStateModel()
+        self.turnTracker = TurnTrackerStateModel(players: self.players)
+        self.rollTracker = RollTrackerStateModel()
+        self.frequencyCounter = FrequencyCounterStateModel()
+    }
+}
---
+++ boardgame dice/RollTrackerStateModel.swift
@@ -0,0 +1,28 @@
+//
+//  RollTrackerStateModel.swift
+//  boardgame dice
+//
+//  Created by James Robinson on 02/11/2020.
+//  Copyright © 2020 James Robinson. All rights reserved.
+//
+
+import Foundation
+
+let distribution: [Int] = [-2,3,3,-4,4,4,5,5,5,5,-6,6,6,6,6,7,7,7,7,7,7,-8,8,8,8,8,9,9,9,9,-10,10,10,11,11,-12]
+
+class RollTrackerStateModel {
+    var rolls: [Int] = distribution.shuffled()
+    
+    func roll(turn: Int) -> Int {
+        return abs(distribution[turn % distribution.count])
+    }
+    
+    func isDouble(turn: Int) -> Bool {
+        return distribution[turn % distribution.count] < 0
+    }
+    
+    func shuffle() {
+        rolls = distribution.shuffled()
+    }
+    
+}
