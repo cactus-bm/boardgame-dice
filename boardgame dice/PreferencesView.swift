6ca0f7c8ae2b9a8681f242edb20ea1a20e5ad039
James Robinson <james@cactus.bm>
2020-12-25 08:39:20 -0400

    Added a preferences view

---
+++ boardgame dice/PreferencesView.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesView.swift
+//  boardgame dice
+//
+//  Created by James Robinson on 25/12/2020.
+//  Copyright © 2020 James Robinson. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text("Preferences")
+    }
+}
+
